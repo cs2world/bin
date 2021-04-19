e72997c1d17a2d8a624c803cb67e9c327a28ad62
PY Cheng <cs2world@gmail.com>
2021-04-19 05:38:48 +0000

    claim defis and defibox shell commands are added

---
+++ claim-defibox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##################################################
+##
+## EOS command line to claim defibox LP rewards
+## version 0.0.1
+## by  PY Cheng
+## April. 19th, 2021
+##
+##################################################
+unlock.sh pool4defibox
+cleos $MU push transaction '{"delay_sec":0,"actions":[{"account":"lptoken.defi","name":"claimall","data":{"owner":"pool4defibox","offset":"BOXGL","limit":20},"authorization":[{"actor":"pool4defibox","permission":"active"}]},{"account":"lppool.defi","name":"claimall","data":{"owner":"pool4defibox"},"authorization":[{"actor":"pool4defibox","permission":"active"}]}]}' 
+
+##################################################
+## offset: BOXL?BXGL
+##################################################
---
+++ claim-defis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##################################################
+##
+## EOS command line to claim defibox LP rewards
+## version 0.0.1
+## by  PY Cheng
+## April. 19th, 2021
+##
+##################################################
+unlock.sh poolfordefis
+cleos $MU push transaction '{"delay_sec":0,"actions":[{"account":"miningpool11","name":"claim","data":{"mid":39,"user":"poolfordefis"},"authorization":[{"actor":"poolfordefis","permission":"active"}]}]}'
+##################################################
